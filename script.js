3485b386c311cfe66bcd4868dc5c05e6797cc445
daliH-4cls <daliah2rnndz10@gmail.com>
2026-03-26 16:27:23 +0000

    addName needs fixing

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Script started");
+
+function start() {
+    console.log("click");
+
+    // Create h1 heading
+    let heading = document.createElement("h1");
+
+    // Set text and its styles
+    heading.innerText = "Attendance";
+    heading.style.color = "green";
+
+    // Add h1 to page
+    document.body.appendChild(heading);
+
+    // Prompting user to add names for attendence
+    prompt("Put in name");
+
+    // Printing name inputted
+    console.log("userName");
+
+    addName(userName);
+}
+
+// Adding names
+function addName(newName) {
+    // Create h2 heading
+    let secondHeading = document.createElement("h2");
+
+    // Set text and its style
+    heading.innerText = "newName";
+
+    // Add h2 to page
+    document.body.appendChild(secondHeading)
+}
